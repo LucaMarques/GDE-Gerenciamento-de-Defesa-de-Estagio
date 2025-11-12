f0c8928cf4fd6f6da256e4844b10a487e39c8e8a
Thatyane Iasmyn Andrade Alves <thatyaneiasmyn1@gmail.com>
2025-11-12 09:59:42 -0300

    atualização

--- src/js/pages/relatorios.js
+++
@@ -1,7 +0,0 @@
-import { getFromStorage } from "../utils/storage";
-import { criarLinhaRelatorio } from "../components/linha.relatorio";
-
-export function renderRelatoriosPages() {
-  const todasDefesas = getFromStorage("Defesasas") || [];
-  const todosUsuários = getFromStorage("usuarios") || [];
-}
