013e2677cd357cbe8d5685382f7324474d3fca5a
Luca Lopes <lucalopesmarques@gmail.com>
2025-11-16 18:25:14 -0300

    Criada pagina de solicitar defesa e funcoes para criar defesa nova no local storage

--- src/data/usuarios.js
+++ src/data/usuarios.js
@@ -5,5 +5,19 @@ export const usuariosBase = [
     "email": "brito.mariana@academico.ifpb.edu.br",
     "senha": "1234",
     "tipo": "coordenador"
-  }
+  },
+  {
+    "nome": "Teste",
+    "matricula": "0000",
+    "email": "teste@gmail.com",
+    "senha": "teste",
+    "tipo": "aluno"
+  },
+  {
+    "nome": "Teste prof",
+    "matricula": "0000",
+    "email": "teste2@gmail.com",
+    "senha": "1234",
+    "tipo": "coordenador"
+  },
 ]
