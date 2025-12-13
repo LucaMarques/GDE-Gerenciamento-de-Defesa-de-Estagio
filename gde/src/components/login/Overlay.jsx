8e0eaa462e74389284aa2ed2700ed1d63999fc1a
Luca Lopes <lucalopesmarques@gmail.com>
2025-12-13 09:29:18 -0300

    Correcao no className

--- gde/src/components/login/Overlay.jsx
+++ gde/src/components/login/Overlay.jsx
@@ -4,17 +4,17 @@ import { useState } from "react";
 
 export default function Overlay({ abrirLogin, abrirCadastro }) {
   return (
-    <div class="overlay-container">
-      <div class="overlay">
-        <h2 class="form-title form-title-light">Já tem conta?</h2>
-        <p class="form-text">Para entrar na nossa plataforma faça login com suas informações</p>
+    <div className="overlay-container">
+      <div className="overlay">
+        <h2 className="form-title form-title-light">Já tem conta?</h2>
+        <p className="form-text">Para entrar na nossa plataforma faça login com suas informações</p>
         <button className="form-button form-button-light" onClick={abrirLogin}>
           Entrar
         </button>
       </div>
-      <div class="overlay">
-        <h2 class="form-title form-title-light">Olá!</h2>
-        <p class="form-text">Cadastre-se e comece a usar a nossa plataforma on-line</p>
+      <div className="overlay">
+        <h2 className="form-title form-title-light">Olá!</h2>
+        <p className="form-text">Cadastre-se e comece a usar a nossa plataforma on-line</p>
         <button className="form-button form-button-light" onClick={abrirCadastro}>
           Cadastrar
         </button>
