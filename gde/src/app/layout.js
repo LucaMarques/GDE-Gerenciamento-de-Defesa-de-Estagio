d6464b6fcca7e45db8002e7cb27c4bc7e217130f
Luca Lopes <lucalopesmarques@gmail.com>
2026-01-28 12:30:54 -0300

    ajustes no css dashboard

--- gde/src/app/layout.js
+++ gde/src/app/layout.js
@@ -26,6 +26,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+        />
+      </head>
       <body>
         <AuthProvider>
           <ModalProvider>
